5368fd7f6d4255173e6e342e1584a25c4526920c
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-04-23 14:04:24 +0300

    Minor improvements

--- Tema3/tasks/tasks/settings.py
+++ Tema3/tasks/tasks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h+296*n114ynbk!%hgak0k74s9zunsd3zr!au%^5ive!6ij$@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Tema3/tasks/tasks/views.py
+++ Tema3/tasks/tasks/views.py
@@ -1,15 +1,12 @@
-# tasks/views.py
-from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from google.cloud import storage, pubsub_v1, firestore
+from google.cloud import storage
+from google.cloud import pubsub_v1
 from google.oauth2 import service_account
 from .models import Task
 from .serializers import TaskSerializer
-
-# Initialize Firestore client with service account credentials and specifying project ID and database ID
-db = firestore.Client.from_service_account_json('key.json', project='emerald-folio-419810', database='cloudhw1')
-
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
@@ -20,31 +17,48 @@ class TaskViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         task = serializer.save()
 
-        # Check if the request time is before May 9, 2024
-        if datetime.now() < datetime(2024, 5, 9):
-            # Upload attachment to Cloud Storage
-            if 'attachment' in self.request.FILES:
-                file = self.request.FILES['attachment']
-                storage_client = storage.Client(credentials=service_account.Credentials.from_service_account_file('key.json'))
-                bucket = storage_client.bucket('my_first_bucket02')
-                blob = bucket.blob('attachments/' + file.name)
-                blob.upload_from_file(file.file, content_type=file.content_type)
-                task.attachment_path = blob.public_url
-                task.save()
+        # Upload attachment to Cloud Storage
+        if 'attachment' in self.request.FILES:
+            file = self.request.FILES['attachment']
+            storage_client = storage.Client(credentials=service_account.Credentials.from_service_account_file('key.json'))
+            bucket = storage_client.bucket('my_first_bucket02')
+            blob = bucket.blob('attachments/' + file.name)
+            blob.upload_from_file(file.file, content_type=file.content_type)
+            task.attachment_path = blob.public_url
+            task.save()
 
-            # Publish message to Cloud Pub/Sub for real-time updates
-            publisher = pubsub_v1.PublisherClient(credentials=service_account.Credentials.from_service_account_file('key.json'))
-            topic_path = publisher.topic_path('emerald-folio-419810', 'my_first_topic')
-            data = {'action': 'create', 'task_id': task.id}
-            message_future = publisher.publish(topic_path, data=str(data).encode('utf-8'))
-            message_future.result()
+        # Publish message to Cloud Pub/Sub for real-time updates
+        publisher = pubsub_v1.PublisherClient(credentials=service_account.Credentials.from_service_account_file('key.json'))
+        topic_path = publisher.topic_path('emerald-folio-419810', 'my_first_topic')
+        data = {'action': 'create', 'task_id': task.id}
+        message_future = publisher.publish(topic_path, data=str(data).encode('utf-8'))
+        message_future.result()
 
-            # Store data in Firestore
-            task_data = serializer.data
-            db.collection('tasks').document(str(task.id)).set(task_data)
-        else:
-            # If the request time is after May 9, 2024, deny the operation
-            return Response({"error": "Operation not allowed after May 9, 2024"}, status=status.HTTP_403_FORBIDDEN)
+        # Add task to Google Sheet
+        scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
+        client = gspread.authorize(credentials)
+        sheet = client.open('CloudHW').sheet1
+        sheet.append_row([task.id, task.title, task.description, task.due_date]) 
 
-    # Other methods perform_update and perform_destroy need similar modifications
+    def perform_update(self, serializer):
+        task = serializer.save()
+
+        # Publish message to Cloud Pub/Sub for real-time updates
+        publisher = pubsub_v1.PublisherClient(credentials=service_account.Credentials.from_service_account_file('key.json'))
+        topic_path = publisher.topic_path('emerald-folio-419810', 'my_first_topic')
+        data = {'action': 'update', 'task_id': task.id}
+        message_future = publisher.publish(topic_path, data=str(data).encode('utf-8'))
+        message_future.result()
+
+    def perform_destroy(self, instance):
+        # Delete the task instance
+        instance.delete()
+        # Publish message to Cloud Pub/Sub for real-time updates
+        publisher = pubsub_v1.PublisherClient(credentials=service_account.Credentials.from_service_account_file('key.json'))
+        topic_path = publisher.topic_path('emerald-folio-419810', 'my_first_topic')
+        data = {'action': 'delete', 'task_id': instance.id}
+        message_future = publisher.publish(topic_path, data=str(data).encode('utf-8'))
+        message_future.result()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
